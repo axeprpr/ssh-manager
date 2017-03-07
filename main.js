a6940653582ca6b8e4b788c4e9bb491ed9436571
espush <webmaster@espush.cn>
2017-03-07 17:38:38 +0800

    using vscode to fix code style.

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const url = require("url");
 
 let win;
 
 function createWindow() {
-	win = new BrowserWindow({
-		width: 850 ,
-		minWidth: 850,
-		height: 440,
-		//frame: false,
-		center: true,
-		autoHideMenuBar: true,
+    win = new BrowserWindow({
+        width: 850,
+        minWidth: 850,
+        height: 440,
+        //frame: false,
+        center: true,
+        autoHideMenuBar: true,
         //transparent: true,
-		show: false
-	});
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, './render.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+        show: false
+    });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, './render.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-	win.once('ready-to-show', () => {
-		win.show();
-	});
-	//win.setMenu(null);
+    win.once('ready-to-show', () => {
+        win.show();
+    });
+    //win.setMenu(null);
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-	if(process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-	if(win === null) {
-		createWindow();
-	}
+    if (win === null) {
+        createWindow();
+    }
 });
-
--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ const webpack = require("webpack");
 
 
 let renderConfig = {
-	entry: {
-		render: './render.js'
-	},
-	target: 'electron-renderer',
-	output: {
-		path: __dirname + "/dist",
-		filename: "[name].bundle.js"
-	},
-	module: {
-		loaders: [
-			{test: /\.css$/, loader: 'style-loader!css-loader'},
-			{test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ['es2015']}},
-		]
-	}
+    entry: {
+        render: './render.js'
+    },
+    target: 'electron-renderer',
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015'] } },
+        ]
+    }
 };
 
 module.exports = [renderConfig];
